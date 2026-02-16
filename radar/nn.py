e12a5e77fc84fb50180196c2acea6dfad2e22bed
Jaiden Magnan <jaidenmagnan@gmail.com>
2026-02-16 15:14:48 -0500

    feat: changing nn

--- radar/nn.py
+++ radar/nn.py
@@ -1,22 +1,34 @@
-## Imports
 from torch import nn
 
 
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv_stack = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Flatten(),
-        )
+class LinearModel(nn.Module):
+    def __init__(self, input_size: int, output_size: int):
+        super(LinearModel, self).__init__()
+        self.bn1 = nn.BatchNorm1d(num_features=input_size)
+        self.fc1 = nn.Linear(in_features=input_size, out_features=64)
+        self.bn2 = nn.BatchNorm1d(num_features=64)
+        self.fc2 = nn.Linear(in_features=64, out_features=32)
+        self.bn3 = nn.BatchNorm1d(num_features=32)
+        self.fc3 = nn.Linear(in_features=32, out_features=16)
+        self.bn4 = nn.BatchNorm1d(num_features=16)
+        self.fc4 = nn.Linear(in_features=16, out_features=output_size)
+        self.relu = nn.ReLU()
 
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(16 * 64 * 4, 512), nn.ReLU(), nn.Linear(512, 2)
-        )
+    def get_weights(self):
+        return self.weight
 
     def forward(self, x):
-        x = self.conv_stack(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        out = self.bn1(x)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.bn2(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.bn3(out)
+        out = self.fc3(out)
+        out = self.bn4(out)
+        out = self.fc4(out)
+
+        out = torch.softmax(out, dim=1)
+
+        return out
